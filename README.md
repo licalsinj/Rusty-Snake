8b23e045933183ba7806091275f8ca486ac4d0b5
licalsinj <licalsinj@gmail.com>
2025-06-28 19:52:05 -0500

    built the game out and added a readme file with instructions to play

---
+++ README.md
@@ -0,0 +1,18 @@
+# Snake in Rust
+
+A snake game built in rust using piston window crate.
+
+## Gameplay
+
+Control the snake with the arrow keys (or WASD) to eat the apple.
+Avoid the walls and running over your own tail.
+End the game by pressing Escape.
+
+## Know Issues
+
+If you press any key other than the arrow keys or WASD.
+
+## Potential Improvements
+
+Instead of auto restarting wait for the user to press space to restart.
+Keep track of the score and display it during the game and at the end.
---
+++ src/draw.rs
@@ -0,0 +1,54 @@
+// Lets you draw rectangles, 2d Graphics
+use piston_window::{rectangle, Context, G2d};
+// Lets you do colors including alpha
+use piston_window::types::Color;
+
+const BLOCK_SIZE: f64 = 25.0;
+
+// takes an integer coordinate on the game screen and returns a scaled location on the board.
+pub fn to_coord(game_coord: i32) -> f64 {
+    (game_coord as f64) * BLOCK_SIZE
+}
+
+pub fn to_coord_u32(game_coord: i32) -> u32 {
+    to_coord(game_coord) as u32
+}
+// draws a block on the screen
+// given a color, coordinate, and the windo context/graphics
+pub fn draw_block(color: Color, x: i32, y: i32, con: &Context, g: &mut G2d) {
+    let gui_x = to_coord(x);
+    let gui_y = to_coord(y);
+
+    rectangle(
+        color,
+        [gui_x, gui_y, BLOCK_SIZE, BLOCK_SIZE],
+        con.transform,
+        g,
+    );
+}
+
+// variation of draw block that takes into account the width & height
+pub fn draw_rectangle(
+    color: Color,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    con: &Context,
+    g: &mut G2d,
+) {
+    let gui_x = to_coord(x);
+    let gui_y = to_coord(y);
+
+    rectangle(
+        color,
+        [
+            gui_x,
+            gui_y,
+            BLOCK_SIZE * (width as f64),
+            BLOCK_SIZE * (height as f64),
+        ],
+        con.transform,
+        g,
+    );
+}
